28e41f8c0f681b331d9707b6a6fc8925a2c1baaf
Krishna <morawala@seas.upenn.edu>
2020-02-14 16:50:30 -0600

    added remaining intents. Most functions are working except for switch category intent

--- lamba_function.py
+++ lamba_function.py
@@ -24,6 +24,7 @@ import random
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+#Added questions in each category, eventually this will be moved to a database
 c_questions = ["What is modular programming?", "What is variable?", "What is the return type of printf() function, and what it returns?", "What is the return type of scanf() function, and what it returns?", "What is the difference between printf() and puts()?"]
 c_plus_plus_questions = ["What is this pointer?"]
 java_questions = ["What is JAVA?", "What are the features in JAVA?", "How does Java enable high performance?", "What are the Java IDE's?", "What do you mean by Constructor?","What is meant by Local variable and Instance variable?","What is a Class?","What is an Object?"]
@@ -43,17 +44,42 @@ class LaunchRequestHandler(AbstractRequestHandler):
         # type: (HandlerInput) -> Response
         speak_output = "Welcome to Practice buddy! I will help you sharpen your interview skills. You can practice questions by saying a category name, like Behavioral, Technical or Coding questions. What would you like to practice?"
         # ask this after user selects the category for the first time.
-        speak_output_return = "Welcome to Practice buddy! I will help you sharpen your interview skills. You can practice questions by saying a category name, like Behavioral, Technical or Coding questions. What would you like to practice?"
+        # speak_output_return = "Welcome to Practice buddy! I will help you sharpen your interview skills. You can practice questions by saying a category name, like Behavioral, Technical or Coding questions. What would you like to practice?"
         # Here are a few things you should know: Your responses will be recorded for you to review during practice sessions. When you are done, say end recording. Let's get started!
         # ask_output = "You can practice questions by saying a category name, like Behavioral, Technical or Coding questions. What would you like to practice?"
 
         return (
             handler_input.response_builder
                 .speak(speak_output)
-                .ask("Say it again?")
+                .ask("Can you please say it again?")
                 .response
         )
 
+class HasCategoryLaunchRequestHandler(AbstractRequestHandler):
+    """Handler for launch after the user have set the interview_category"""
+
+    def can_handle(self, handler_input):
+        # extract persistent attributes and check if they are all present
+        attr = handler_input.attributes_manager.persistent_attributes
+        attributes_are_present = ("interview_category" in attr)
+
+        return attributes_are_present and ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        #retrieve the attribute
+        attr = handler_input.attributes_manager.persistent_attributes
+        category = attr['interview_category']
+
+        speak_output = "Welcome back to Practice buddy. Want to keep practicing {category}? If yes, say next question or to switch say switch category!".format(category=category)
+        # handler_input.response_builder.speak(speak_output)
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask("Can you please say it again?")
+                .response
+        )
+        # return handler_input.response_builder.response
 
 class CaptureCategoryIntentHandler(AbstractRequestHandler):
     """Handler for CaptureCategoryIntent Intent."""
@@ -68,13 +94,13 @@ class CaptureCategoryIntentHandler(AbstractRequestHandler):
         #specifically extracting INTERVIEW_CATEGORY
         category = slots["INTERVIEW_CATEGORY"].value
 
-        #not sure???
+        #saving the interview category in the "interview_category" persistent_attribute
         attributes_manager = handler_input.attributes_manager
         category_attributes = {
             "interview_category": category
         }
         attributes_manager.persistent_attributes = category_attributes
-        # attributes_manager.save_persistent_attributes()
+        attributes_manager.save_persistent_attributes()
 
         # type: (HandlerInput) -> Response
         question = " "
@@ -97,8 +123,8 @@ class CaptureCategoryIntentHandler(AbstractRequestHandler):
             question = random.choice(coding_questions)
 
         # question = python_questions[0]
-
-        speak_output = "Ok, Here you go - a question in {category}. ".format(category=category) + " " +  question + " " + "Your time starts now!"
+        # speak_output = "Ok, Here you go - a question in {category}. ".format(category=category) + " " +  question + " " + "Say next question!"
+        speak_output = "Ok, Here is a question. " +  question + " " + "When done, say next question or switch category!"
 
         return (
             handler_input.response_builder
@@ -108,24 +134,122 @@ class CaptureCategoryIntentHandler(AbstractRequestHandler):
                 .response
         )
 
-class HasCategoryLaunchRequestHandler(AbstractRequestHandler):
+class SwitchCategoryIntentHandler(AbstractRequestHandler):
+    """Handler for SwitchCategoryIntent Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("SwitchCategoryIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # bringing in the slot values
+        slots = handler_input.request_envelope.request.intent.slots
+
+        if slots is not "":
+            #specifically extracting INTERVIEW_CATEGORY
+            category = slots["INTERVIEW_CATEGORY"].value
+
+            #saving the interview category in the "interview_category" persistent_attribute
+            attributes_manager = handler_input.attributes_manager
+            category_attributes = {
+                "interview_category": category
+            }
+            attributes_manager.persistent_attributes = category_attributes
+            attributes_manager.save_persistent_attributes()
+
+        # type: (HandlerInput) -> Response
+        question = " "
+
+        if category is 'C programming language':
+            question = random.choice(c_questions)
+        elif category is 'C plus plus':
+            question = random.choice(c_plus_plus_questions)
+        elif category is 'coding':
+            question = random.choice(coding_questions)
+        elif category is 'behavioral':
+            question = random.choice(behavioral_questions)
+        elif category is 'technical':
+            question = random.choice(technical_questions)
+        elif category is 'python':
+            question = random.choice(python_questions)
+        elif category is 'java':
+            question = random.choice(java_questions)
+        else:
+            question = random.choice(coding_questions)
+
+        speak_output = ""
+        if slots is "":
+            speak_output = "Ok, which category would you like?"
+        else:
+            # speak_output = "Ok, Here you go - a question in {category}. ".format(category=category) + " " +  question + " " + "When done, say next question or switch category!"
+            speak_output = "Ok, Here is a question. " +  question + " " + "When done, say next question or switch category!"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .speak(question)
+                .ask("Say it again?")
+                .response
+        )
+
+class AskNextQuestionIntentHandler(AbstractRequestHandler):
     """Handler for launch after the user have set the interview_category"""
 
     def can_handle(self, handler_input):
+
+        # attr = handler_input.attributes_manager.persistent_attributes
+        # attributes_are_present = ("interview_category" in attr)
+
+        return ask_utils.is_intent_name("AskNextQuestionIntent")(handler_input)
+
+    def handle(self, handler_input):
+
         # extract persistent attributes and check if they are all present
         attr = handler_input.attributes_manager.persistent_attributes
         attributes_are_present = ("interview_category" in attr)
 
-        return attributes_are_present and ask_utils.is_request_type("LaunchRequest")(handler_input)
-
-    def handle(self, handler_input):
         attr = handler_input.attributes_manager.persistent_attributes
-        category = attr['interview_category']
+        if attributes_are_present:
+            category = attr['interview_category']
 
-        speak_output = "Welcome back to Practice buddy. Want to keep practicing {category} category?".format(category=category)
-        handler_input.response_builder.speak(speak_output)
+        # attr = handler_input.attributes_manager.persistent_attributes
 
-        return handler_input.response_builder.response
+        # extract persistent attributes and check if they are all present
+        # attr = handler_input.attributes_manager.persistent_attributes
+        # attributes_are_present = ("interview_category" in attr)
+
+        # attr = handler_input.attributes_manager.persistent_attributes
+        # category = attr['interview_category']
+
+        # type: (HandlerInput) -> Response
+        question = " "
+
+        if category is 'C programming language':
+            question = random.choice(c_questions)
+        elif category is 'C plus plus':
+            question = random.choice(c_plus_plus_questions)
+        elif category is 'coding':
+            question = random.choice(coding_questions)
+        elif category is 'behavioral':
+            question = random.choice(behavioral_questions)
+        elif category is 'technical':
+            question = random.choice(technical_questions)
+        elif category is 'python':
+            question = random.choice(python_questions)
+        elif category is 'java':
+            question = random.choice(java_questions)
+        else:
+            question = random.choice(coding_questions)
+
+        # speak_output = "Here is a question in {category}. ".format(category=category) + " " +  question + " " + "When done, say next question or switch category!"
+        speak_output = "Ok, Here is a question. " +  question + " " + "When done, say next question or switch category!"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask("Say it again?")
+                .response
+        )
 
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
@@ -229,10 +353,9 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 
 #sb = SkillBuilder()
 sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
-
-# sb.add_request_handler(HasCategoryLaunchRequestHandler())
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(CaptureCategoryIntentHandler())
+sb.add_request_handler(AskNextQuestionIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
